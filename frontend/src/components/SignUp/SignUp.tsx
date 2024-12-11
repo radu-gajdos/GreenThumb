41b2bcf601452252695543ac271342b9f801ee85
radu-gajdos <radugajdos123@gmail.com>
2024-12-12 00:15:45 +0200

    Signup component frontend NO FUNCTIONALITY V1

--- frontend/src/components/SignUp/SignUp.tsx
+++ frontend/src/components/SignUp/SignUp.tsx
@@ -7,6 +7,7 @@ const SignUp: React.FC = () => {
         username: "",
         email: "",
         password: "",
+        repassword: "",
     });
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,7 +23,6 @@ const SignUp: React.FC = () => {
     return (
         <div className={styles.main}>
             <div className={styles.formContainer}>
-                <Circle className={styles.circle} />
                 <div className={styles.formContent}>
                     <div className={styles.formTitle}>Sign Up</div>
                     <form onSubmit={handleSubmit} className={styles.form}>
@@ -32,46 +32,108 @@ const SignUp: React.FC = () => {
                         </div>
                         <div className={styles.input}>
                             <div className={styles.inputTitle}>Username</div>
-                                <input
-                                    className={styles.inputSlot}
-                                    type="text"
-                                    name="username"
-                                    value={formData.username}
-                                    onChange={handleChange}
-                                    required
-                                />
+                            <input
+                                className={styles.inputSlot}
+                                type="text"
+                                name="username"
+                                value={formData.username}
+                                onChange={handleChange}
+                                required
+                            />
                         </div>
                         <div className={styles.input}>
                             <div className={styles.inputTitle}>Email</div>
-                            <div className={styles.inputSlot}>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
+                            <input
+                                className={styles.inputSlot}
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                required
+                            />
                         </div>
                         <div className={styles.input}>
                             <div className={styles.inputTitle}>Password</div>
-                            <div className={styles.inputSlot}>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    value={formData.password}
-                                    onChange={handleChange}
-                                    required
-                                />
+                            <input
+                                className={styles.inputSlot}
+                                type="password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className={styles.input}>
+                            <div className={styles.inputTitle}>
+                                Repeat Password
                             </div>
+                            <input
+                                className={styles.inputSlot}
+                                type="repassword"
+                                name="repassword"
+                                value={formData.repassword}
+                                onChange={handleChange}
+                                required
+                            />
                         </div>
                         <button type="submit" className={styles.submitButton}>
                             Sign Up
                         </button>
                     </form>
+                    <div className={styles.accountCheck}>Already have an account? Log in</div>
                 </div>
             </div>
-            <div className={styles.ilustrationContainer}>asda</div>
+            <div className={styles.ilustrationContainer}>
+                <div className={styles.ilustrationContent}>
+                    <div className={styles.ilustrationTitle}>
+                        A Platform Designed for You
+                    </div>
+                    <div className={styles.ilustrationBullets}>
+                        <div className={styles.ilustrationBullet}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="9"
+                                height="8"
+                                viewBox="0 0 9 8"
+                                fill="none"
+                            >
+                                <circle cx="4.5" cy="4" r="4" fill="#3F3D56" />
+                            </svg>
+                            <div className={styles.ilustrationBulletText}>
+                                Customizable Garden Pages
+                            </div>
+                        </div>
+                        <div className={styles.ilustrationBullet}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="9"
+                                height="8"
+                                viewBox="0 0 9 8"
+                                fill="none"
+                            >
+                                <circle cx="4.5" cy="4" r="4" fill="#3F3D56" />
+                            </svg>
+                            <div className={styles.ilustrationBulletText}>
+                                AI-Powered Assistance
+                            </div>
+                        </div>
+                        <div className={styles.ilustrationBullet}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="9"
+                                height="8"
+                                viewBox="0 0 9 8"
+                                fill="none"
+                            >
+                                <circle cx="4.5" cy="4" r="4" fill="#3F3D56" />
+                            </svg>
+                            <div className={styles.ilustrationBulletText}>
+                                Community Connection
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
