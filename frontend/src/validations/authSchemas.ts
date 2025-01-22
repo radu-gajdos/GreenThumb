e3e0e47c24134bc3e22f5eeaf408c9446df5068a
radu-gajdos <radugajdos123@gmail.com>
2025-01-22 22:23:13 +0200

    Refactor SignUp component: Extract validation schemas, API logic, and custom hooks

--- frontend/src/validations/authSchemas.ts
+++ frontend/src/validations/authSchemas.ts
@@ -1,6 +1,5 @@
 import * as yup from "yup";
 
-// A factory function to generate the validation schema
 export const getSignInValidationSchema = (t: (key: string) => string) =>
     yup.object().shape({
         email: yup
@@ -8,4 +7,23 @@ export const getSignInValidationSchema = (t: (key: string) => string) =>
             .email(t("validation.invalidEmail"))
             .required(t("validation.emailRequired")),
         password: yup.string().required(t("validation.passwordRequired")),
+    });
+
+export const getSignUpValidationSchema = (
+    t: (key: string, options?: Record<string, any>) => string
+) =>
+    yup.object().shape({
+        username: yup.string().required(t("validation.usernameRequired")),
+        email: yup
+            .string()
+            .email(t("validation.invalidEmail"))
+            .required(t("validation.emailRequired")),
+        password: yup
+            .string()
+            .required(t("validation.passwordRequired"))
+            .min(8, t("validation.passwordMin")),
+        repassword: yup
+            .string()
+            .required(t("validation.repasswordRequired"))
+            .oneOf([yup.ref("password")], t("validation.passwordMatch")),
     });
